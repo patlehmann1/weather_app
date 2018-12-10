b7ee48c4be6c013eb2c8e66331216ebe7c5dc1ae
pat <patlehmann1@gmail.com>
2018-12-10 00:06:44 -0500

    Updates

---
+++ precache-manifest.68c3b6e4ecbfce95f09797a2adc08b4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "53c1ec5f96d716d4265e536f7576919e",
+    "url": "/weather_app/static/media/bright-squares.53c1ec5f.png"
+  },
+  {
+    "revision": "2b8e50d38204ea6f6971",
+    "url": "/weather_app/static/js/runtime~main.2b8e50d3.js"
+  },
+  {
+    "revision": "d25c85d11561fbe104f6",
+    "url": "/weather_app/static/js/main.d25c85d1.chunk.js"
+  },
+  {
+    "revision": "2818dbc80ba3c939eefa",
+    "url": "/weather_app/static/js/1.2818dbc8.chunk.js"
+  },
+  {
+    "revision": "d25c85d11561fbe104f6",
+    "url": "/weather_app/static/css/main.3e1a2422.chunk.css"
+  },
+  {
+    "revision": "ac297489ee44c2852153ca4e2d09c8f7",
+    "url": "/weather_app/index.html"
+  }
+];
